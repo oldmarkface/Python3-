003ae71d26253deae622737b597207544ec096de
Mark <1782980833@qq.com>
2019-06-25 12:08:59 +0800

    二分法、选择排序

--- 2、选择排序.py
+++ 2、选择排序.py
@@ -1,20 +1,22 @@
 def find_smallest(arr):
     # 获取最小元素的小表
-    smallest=arr[0]
-    smallest_index=0
-    for index in range(1,len(arr)):
-        if arr[index]<smallest:
-            smallest=arr[index]
-            smallest_index=index
+    smallest = arr[0]
+    smallest_index = 0
+    for index in range(1, len(arr)):
+        if arr[index] < smallest:
+            smallest = arr[index]
+            smallest_index = index
     return smallest_index
+
 
 def selection_sort(arr):
     # 选择排序
-    new_arr=[]
+    new_arr = []
     for index in range(len(arr)):
-        smallest=find_smallest(arr)
+        smallest = find_smallest(arr)
         new_arr.append(arr.pop(smallest))
     return new_arr
 
+
 if __name__ == '__main__':
-    print(selection_sort([1,3,5,4,7,9,2,8]))
+    print(selection_sort([1, 3, 5, 4, 7, 9, 2, 8]))
